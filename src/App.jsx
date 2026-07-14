1bb4ef61e350bf466bbb3c3d4ef347b3219bcf52
antoniocantillo1988 <loko_acr@hotmail.com>
2026-07-14 20:14:27 +0200

    fallo en layout

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,6 @@
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import Layout from './components/Layout';
-import Home from './pages/Home';
+import Home from './components/Home';
 
 export default function App() {
   return (
