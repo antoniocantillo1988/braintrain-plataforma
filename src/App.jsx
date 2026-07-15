00b05cca0fc8060653e86d4a09e5052895a1ef9f
antoniocantillo1988 <loko_acr@hotmail.com>
2026-07-15 16:37:38 +0200

    actualización Layout

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,15 @@
+// src/App.jsx
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import Layout from './components/Layout';
-import Home from './components/Home';
+import Home from './pages/Home'; // <--- ESTO DEBE COINCIDIR CON LA CARPETA src/pages/
+import Citas from './pages/Citas';
 
 export default function App() {
   return (
     <BrowserRouter>
       <Routes>
         <Route path="/" element={<Layout><Home /></Layout>} />
+        <Route path="/citas" element={<Layout><Citas /></Layout>} />
       </Routes>
     </BrowserRouter>
   );
